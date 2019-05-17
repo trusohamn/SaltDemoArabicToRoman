7bcca21081ce191dde685723b1b83dbb6b7edc82
trusohamn <marta.trusohamn@gmail.com>
2019-05-17 09:25:56 +0200

    wrapping with numberConverter

--- index.js
+++ index.js
@@ -19,4 +19,20 @@ function digitConverter(dig, digIndex) {
 
     return output;
 }
+
+function numberConverter(number) { // 25
+    const numStr = number.toString();
+    let result = [];
+    let digIndex = 0;
+    for (let i = numStr.length - 1; i >= 0; i--) {
+        let y = digitConverter(parseInt(numStr.charAt(i)), digIndex); // digitConverter(5,0) = 'V' ; dC(2,1)='II'
+        result.unshift(y); // ['II', V']
+        digIndex++;
+    }
+    return result.join('');
+}
+
+
 module.exports.digitConverter = digitConverter;
+module.exports.numberConverter = numberConverter;
+
